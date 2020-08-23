0f6fb58c24b6bec288d73bd0784d6c3dec2ee804
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-23 19:29:44 +0200

    rename project

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/dawidd6/p2p
+module github.com/dawidd6/peerty
 
 go 1.14
 
-require github.com/spf13/cobra v1.0.0 // indirect
+require github.com/spf13/cobra v1.0.0
--- main.go
+++ main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import "github.com/spf13/cobra"
 
+var cmdRoot = &cobra.Command{
+	Use:                        "peerty",
+	Example:                    "",
+	Version:                    "0.1.0",
+	PersistentPreRun:           func(*cobra.Command, []string) { panic("not implemented") },
+	PersistentPreRunE:          func(*cobra.Command, []string) error { panic("not implemented") },
+	PreRun:                     func(*cobra.Command, []string) { panic("not implemented") },
+	PreRunE:                    func(*cobra.Command, []string) error { panic("not implemented") },
+	Run:                        func(*cobra.Command, []string) { panic("not implemented") },
+	RunE:                       func(*cobra.Command, []string) error { panic("not implemented") },
+	PostRun:                    func(*cobra.Command, []string) { panic("not implemented") },
+	PostRunE:                   func(*cobra.Command, []string) error { panic("not implemented") },
+	PersistentPostRun:          func(*cobra.Command, []string) { panic("not implemented") },
+	PersistentPostRunE:         func(*cobra.Command, []string) error { panic("not implemented") },
+	SilenceErrors:              false,
+	SilenceUsage:               false,
+	DisableFlagParsing:         false,
+	DisableAutoGenTag:          false,
+	DisableFlagsInUseLine:      false,
+	DisableSuggestions:         false,
+	SuggestionsMinimumDistance: 0,
+	TraverseChildren:           false,
+}
+
+var cmdDaemon = &cobra.Command{
+	Use:                        "daemon",
+	Example:                    "",
+	PersistentPreRun:           func(*cobra.Command, []string) { panic("not implemented") },
+	PersistentPreRunE:          func(*cobra.Command, []string) error { panic("not implemented") },
+	PreRun:                     func(*cobra.Command, []string) { panic("not implemented") },
+	PreRunE:                    func(*cobra.Command, []string) error { panic("not implemented") },
+	Run:                        func(*cobra.Command, []string) { panic("not implemented") },
+	RunE:                       func(*cobra.Command, []string) error { panic("not implemented") },
+	PostRun:                    func(*cobra.Command, []string) { panic("not implemented") },
+	PostRunE:                   func(*cobra.Command, []string) error { panic("not implemented") },
+	PersistentPostRun:          func(*cobra.Command, []string) { panic("not implemented") },
+	PersistentPostRunE:         func(*cobra.Command, []string) error { panic("not implemented") },
+	SilenceErrors:              false,
+	SilenceUsage:               false,
+	DisableFlagParsing:         false,
+	DisableAutoGenTag:          false,
+	DisableFlagsInUseLine:      false,
+	DisableSuggestions:         false,
+	SuggestionsMinimumDistance: 0,
+	TraverseChildren:           false,
+}
+
+func init() {
+	cmdRoot.AddCommand(
+		cmdDaemon,
+	)
+}
+
+func main() {
+	err := cmdRoot.Execute()
+	if err != nil {
+		panic(err)
+	}
 }
