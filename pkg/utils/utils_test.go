464f102c0a80f08759e0c07dff286f21f9bf8cef
Dawid Dziurla <dawidd0811@gmail.com>
2020-10-14 13:23:08 +0200

    utils: add more tests

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -12,3 +12,30 @@ func TestSha256(t *testing.T) {
 
 	assert.Equal(t, got, want)
 }
+
+// dd bs=1 skip=16 count=16 if=test/fixtures/go.sum 2>/dev/null
+func TestReadFilePiece(t *testing.T) {
+	piece, err := ReadFilePiece("../../test/fixtures/go.sum", 16, 0)
+	want := "442f9ad4aedc52dca7bf9219c7fb48a4906afb8877b21120cf8d68281704dfc8"
+	got := Sha256Sum(piece)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, piece)
+	assert.Equal(t, got, want)
+
+	piece, err = ReadFilePiece("../../test/fixtures/go.sum", 16, 10)
+	want = "019c4df04108da14cc6d0f3f628c7935c7e423c4b3839e559266f9178a74c62e"
+	got = Sha256Sum(piece)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, piece)
+	assert.Equal(t, got, want)
+}
+
+func TestDoInDirectory(t *testing.T) {
+	err := DoInDirectory("", func() error {
+		return nil
+	})
+
+	assert.NoError(t, err)
+}
