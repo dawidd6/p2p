10eb848acc8cae6fcffe4e2e113fcd0870946c66
Dawid Dziurla <dawidd0811@gmail.com>
2020-10-18 17:15:44 +0200

    defaults: add more constants

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -1,9 +1,14 @@
 package defaults
 
+import "time"
+
 const (
 	TrackerListenAddress = "127.0.0.1:8889"
 	DaemonListenAddress  = "127.0.0.1:8888"
 	SeedListenAddress    = "0.0.0.0:44444"
+
+	TrackerAnnounceInterval = time.Second * 30
+	TrackerCleanInterval    = time.Second * 60
 
 	PieceSize = 256 * 1024 // 256 kB
 )
