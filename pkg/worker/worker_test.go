6f591780c07c3cf13a8b8ecfee92bce37f06a9df
Dawid Dziurla <dawidd0811@gmail.com>
2020-10-30 13:26:52 +0100

    worker: add test

---
+++ pkg/worker/worker_test.go
@@ -0,0 +1,24 @@
+package worker_test
+
+import (
+	"sync/atomic"
+	"testing"
+
+	"github.com/dawidd6/p2p/pkg/worker"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	got := uint32(0)
+	expected := uint32(10)
+	pool := worker.New(2)
+	pool.Start()
+	for i := uint32(0); i < expected; i++ {
+		pool.Enqueue(func() {
+			atomic.AddUint32(&got, 1)
+		})
+	}
+	pool.Stop()
+
+	assert.Equal(t, expected, got)
+}
